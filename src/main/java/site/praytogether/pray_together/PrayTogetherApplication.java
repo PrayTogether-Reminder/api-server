7c6859580ce6a8aec544ed8b293c742ea536f81e
kchranger <kchranger@gmail.com>
2025-02-12 21:43:34 +0900

    chore: project 초기화

---
+++ src/main/java/site/praytogether/pray_together/PrayTogetherApplication.java
@@ -0,0 +1,13 @@
+package site.praytogether.pray_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrayTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrayTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/praytogether/pray_together/PrayTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package site.praytogether.pray_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrayTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
