f3209200f86cc83fa8c7800a5e4675875a38bab4
kchranger <kchranger@gmail.com>
2025-03-31 14:10:36 +0900

    feat:  BaseEntity 및 Audit 설정 #7

---
+++ src/main/java/site/praytogether/pray_together/domain/base/BaseEntity.java
@@ -0,0 +1,34 @@
+package site.praytogether.pray_together.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_time", updatable = false, columnDefinition = "TIMESTAMP(3)")
+  private Instant createdTime;
+
+  @CreatedBy
+  @Column(name = "created_by")
+  private Long createdBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_time", columnDefinition = "TIMESTAMP(3)")
+  private Instant updatedTime;
+
+  @LastModifiedBy
+  @Column(name = "updated_By")
+  private Long updatedBy;
+}
