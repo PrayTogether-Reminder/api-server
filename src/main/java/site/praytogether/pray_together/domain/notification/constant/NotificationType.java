07f51f423cb515d7fe42a6df0f008fcab51db6c8
kchranger <kchranger@gmail.com>
2025-04-15 21:15:16 +0900

    feat: Notification 기본 Entity 정의 #19

---
+++ src/main/java/site/praytogether/pray_together/domain/notification/constant/NotificationType.java
@@ -0,0 +1,11 @@
+package site.praytogether.pray_together.domain.notification.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum NotificationType {
+  PRAYER_COMPLETION,
+  ;
+}
