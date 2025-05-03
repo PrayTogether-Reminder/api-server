e1be7ee9ed2641469930701b126c3ed0be06e569
kchranger <kchranger@gmail.com>
2025-05-03 23:58:04 +0900

    fix: 방에 존재하는 회원 초대를 막는 로직 추가 #25
    - MemberRoom의 Member Id, Room Id를 Unique로 설정

--- src/main/java/site/praytogether/pray_together/domain/notification/constant/NotificationType.java
+++ src/main/java/site/praytogether/pray_together/domain/notification/constant/NotificationType.java
@@ -1,10 +1,6 @@
 package site.praytogether.pray_together.domain.notification.constant;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
 public class NotificationType {
   public static final String PRAYER_COMPLETION = "PRAYER_COMPLETION";
 }
