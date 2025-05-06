535366ab587c8337045187236eb39a657f4a4ade
kchranger <kchranger@gmail.com>
2025-05-06 20:41:38 +0900

    feat: Firebase Config 설정 추가 #27

---
+++ src/main/java/site/praytogether/pray_together/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package site.praytogether.pray_together.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class FirebaseConfig {
+
+  @Bean
+  FirebaseApp firebaseApp() throws IOException {
+    ClassPathResource resource = new ClassPathResource("pray-together-firebase-adminsdk.json");
+    InputStream serviceAccount = resource.getInputStream();
+
+    FirebaseOptions options =
+        FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+    return FirebaseApp.initializeApp(options);
+  }
+
+  @Bean
+  FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+    return FirebaseMessaging.getInstance(firebaseApp);
+  }
+}
