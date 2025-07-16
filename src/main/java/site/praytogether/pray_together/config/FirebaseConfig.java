b2b060d9d04855078dc8849cc23798df104f51f8
kchranger <kchranger@gmail.com>
2025-07-17 00:21:14 +0900

    chore: firebase 파일 path 입력 방법 변경

--- src/main/java/site/praytogether/pray_together/config/FirebaseConfig.java
+++ src/main/java/site/praytogether/pray_together/config/FirebaseConfig.java
@@ -4,8 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,10 +17,13 @@ import org.springframework.core.io.ClassPathResource;
 @Profile("!test")
 public class FirebaseConfig {
 
+  @Value("${firebase.sdk.path}")
+  private String firebaseSdkPath;
+
   @Bean
+
   FirebaseApp firebaseApp() throws IOException {
-    ClassPathResource resource = new ClassPathResource("pray-together-firebase-adminsdk.json");
-    InputStream serviceAccount = resource.getInputStream();
+    FileInputStream serviceAccount = new FileInputStream(firebaseSdkPath);
 
     FirebaseOptions options =
         FirebaseOptions.builder()
