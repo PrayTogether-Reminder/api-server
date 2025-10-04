52fcc92ddb8dfdf0fd8732cae5ded48e117e0e81
kchranger <kchranger@gmail.com>
2025-10-04 18:44:06 +0900

    feat: 기도방 초대시 중복 초대 제외 로직 추가 #37
    - 기도 방에 없으면서, 아직 초대를 받고 응답을 안한 경우 중복 초대 안함.

--- src/main/java/site/praytogether/pray_together/domain/invitation/domain/service/InvitationService.java
+++ src/main/java/site/praytogether/pray_together/domain/invitation/domain/service/InvitationService.java
@@ -1,6 +1,10 @@
 package site.praytogether.pray_together.domain.invitation.domain.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,8 +40,18 @@ public class InvitationService {
 
   public List<Invitation> create(Member inviter, List<Member> invitees, Room room) {
 
+    List<Long> inviteeIds = invitees.stream().map(Member::getId).toList();
+    List<Invitation> pendingInvitations =
+        invitationRepository.findByRoomIdAndStatusAndInviteeIds(room.getId(),InvitationStatus.PENDING,inviteeIds);
+
+    Set<Long> pendingIds = pendingInvitations.stream()
+        .map(Invitation::getInvitee)
+        .map(Member::getId)
+        .collect(Collectors.toSet());
+
     List<Invitation> invitations = invitees.stream()
-        .map(i -> Invitation.create(inviter, i, room))
+        .filter(invitee -> !pendingIds.contains(invitee.getId())) // not contains
+        .map(invitee -> Invitation.create(inviter, invitee, room))
         .toList();
 
     return invitationRepository.saveAll(invitations);
