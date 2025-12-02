3cec9e21af42e0adbef46b7aaa10445a70048c3d
kchranger <kchranger@gmail.com>
2025-12-02 19:48:38 +0900

    test: 통합 테스트 트랜잭션 범위 변경 #46
    - 테스트 클래스에 Tx를 제외하고, mock 서버 비즈니스 로직의 Tx를 활용
    - 테스트 종료시, 항상 DB 테이블 초기화 로직 적용
    - 로그아웃 필터 테스트를 명확하게 하기 위해 트랜잭션 범위 변경

--- src/test/java/site/praytogether/pray_together/test_config/IntegrateTest.java
+++ src/test/java/site/praytogether/pray_together/test_config/IntegrateTest.java
@@ -1,13 +1,16 @@
 package site.praytogether.pray_together.test_config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import site.praytogether.pray_together.domain.auth.repository.RefreshTokenRepository;
 import site.praytogether.pray_together.domain.friend.domain.friend_invitation.FriendInvitationRepository;
 import site.praytogether.pray_together.domain.friend.domain.friendship.FriendshipRepository;
@@ -23,7 +26,6 @@ import site.praytogether.pray_together.domain.room.repository.RoomRepository;
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 @Import(value = IntegrateTestConfig.class)
 public class IntegrateTest {
   @Autowired protected MockMvc mockMvc;
@@ -44,6 +46,9 @@ public class IntegrateTest {
 
   @Autowired protected TestUtils testUtils;
 
+  @PersistenceContext protected EntityManager entityManager;
+  @Autowired protected TransactionTemplate transactionTemplate;
+
   protected final String API_VERSION_1 = "/api/v1";
   protected String API_VERSION_2 = "/api/v2";
   protected final String AUTH_API_URL = API_VERSION_1 + "/auth";
@@ -52,4 +57,22 @@ public class IntegrateTest {
   protected final String MEMBERS_API_URL = API_VERSION_1 + "/members";
   protected final String INVITATIONS_API_URL = API_VERSION_1 + "/invitations";
   protected final String FRIEND_API_URL = API_VERSION_1 + "/friends";
+
+  @AfterEach
+  void tearDown() {
+    transactionTemplate.execute(status -> { // hibernate -> Insert, Update, Delete need Tx
+      entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+      entityManager.getMetamodel().getEntities().forEach(entityType -> {
+        String tableName = entityType.getName();
+        entityManager.createQuery("DELETE FROM " + tableName).executeUpdate();
+      });
+
+      entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+      entityManager.flush();
+      entityManager.clear();
+
+      return null;
+    });
+  }
 }
